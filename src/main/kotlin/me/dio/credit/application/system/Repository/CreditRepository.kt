afe428ce53e896ce641fc100e8ee5c9f8ca80ba7
Tiago061 <Thenriqueleite277@gmail.com>
2024-02-24 15:58:14 -0300

    Implementação da interface(save, findByCreditCode)

--- src/main/kotlin/me/dio/credit/application/system/Repository/CreditRepository.kt
+++ src/main/kotlin/me/dio/credit/application/system/Repository/CreditRepository.kt
@@ -1,8 +1,12 @@
 package me.dio.credit.application.system.Repository
 
 import me.dio.credit.application.system.entity.Credit
+import me.dio.credit.application.system.service.impl.CreditService
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
-interface CrediteRepository: JpaRepository<Credit, Long>
+interface CreditRepository: JpaRepository<Credit, Long> {
+    fun findByCreditCode(creditCode: UUID): Credit?
+}
